df0350e275a78af7130b84f8a8f1b4d3123d35a2
Azim Akbay <azimakbay@Mac.gitbo.lan>
2026-04-30 09:46:00 +0200

    db efficientie

--- controller/controller.py
+++ controller/controller.py
@@ -2,73 +2,94 @@ class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-        
         self.view.set_controller(self)
-        self.load_initial_data()
+        self.refresh_all_tables()
 
-    def load_initial_data(self):
-        """Laadt de data in bij het opstarten."""
-        self.refresh_student_table()
-
-    def refresh_student_table(self):
-        """Haalt de studenten op uit de DB en toont ze in de tabel."""
+    def refresh_all_tables(self):
+        """Haalt alle data op en vult de schermen en comboboxes."""
         studenten = self.model.get_all_students()
-        self.view.populate_student_tree(studenten)
+        transports = self.model.get_all_transports()
+        logs = self.model.get_all_logs()
 
-    # --- Studenten CRUD Acties ---
+        self.view.populate_tree(self.view.tree_students, studenten)
+        self.view.populate_tree(self.view.tree_trans, transports)
+        self.view.populate_tree(self.view.tree_logs, logs)
+
+        # Vul de dropdown menu's (Comboboxes) op de Logs-pagina
+        self.view.combo_student['values'] = [f"{s[0]} - {s[1]}" for s in studenten]
+        self.view.combo_vervoer['values'] = [f"{t[0]} - {t[1]}" for t in transports]
+
+    # --- Studenten ---
     def add_student(self):
         data = self.view.get_student_form_data()
-        
-        # Validatie: Controleer of alle velden zijn ingevuld
-        if not data['naam'] or not data['klas'] or not data['afstand']:
-            self.view.show_error("Vul a.u.b. alle velden in!")
-            return
-        
+        if not all(data.values()): return self.view.show_error("Vul alles in!")
         try:
-            # Vervang eventueel een komma door een punt en zet om naar kommagetal
-            afstand = float(data['afstand'].replace(',', '.'))
-            
-            # Opslaan in de database
-            self.model.add_student(data['naam'], data['klas'], afstand)
-            
-            # View updaten
+            self.model.add_student(data['naam'], data['klas'], float(data['afstand'].replace(',', '.')))
             self.view.clear_student_form()
-            self.refresh_student_table()
-            self.view.show_info(f"Student '{data['naam']}' is toegevoegd.")
-        except ValueError:
-            self.view.show_error("De afstand moet een geldig getal zijn.")
+            self.refresh_all_tables()
+            self.view.show_info("Student toegevoegd.")
+        except ValueError: self.view.show_error("Ongeldige afstand.")
 
     def update_student(self):
-        student_id = self.view.get_selected_student_id()
-        if not student_id:
-            self.view.show_error("Selecteer eerst een student uit de tabel om aan te passen.")
-            return
-
+        student_id = self.view.get_selected_id(self.view.tree_students)
+        if not student_id: return self.view.show_error("Selecteer een student.")
         data = self.view.get_student_form_data()
-        if not data['naam'] or not data['klas'] or not data['afstand']:
-            self.view.show_error("Vul a.u.b. alle velden in!")
-            return
-        
+        if not all(data.values()): return self.view.show_error("Vul alles in!")
         try:
-            afstand = float(data['afstand'].replace(',', '.'))
-            self.model.update_student(student_id, data['naam'], data['klas'], afstand)
-            
+            self.model.update_student(student_id, data['naam'], data['klas'], float(data['afstand'].replace(',', '.')))
             self.view.clear_student_form()
-            self.refresh_student_table()
-            self.view.show_info(f"Studentgegevens aangepast.")
-        except ValueError:
-            self.view.show_error("De afstand moet een geldig getal zijn.")
+            self.refresh_all_tables()
+            self.view.show_info("Student aangepast.")
+        except ValueError: self.view.show_error("Ongeldige afstand.")
 
     def delete_student(self):
-        student_id = self.view.get_selected_student_id()
-        if not student_id:
-            self.view.show_error("Selecteer eerst een student uit de tabel om te verwijderen.")
-            return
-        
+        student_id = self.view.get_selected_id(self.view.tree_students)
+        if not student_id: return self.view.show_error("Selecteer een student.")
         self.model.delete_student(student_id)
         self.view.clear_student_form()
-        self.refresh_student_table()
-        self.view.show_info("Student succesvol verwijderd. Alle bijbehorende logs zijn ook verwijderd om dataconsistentie te garanderen.")
+        self.refresh_all_tables()
+        self.view.show_info("Student en bijbehorende logs verwijderd.")
+
+    # --- Vervoer ---
+    def add_transport(self):
+        type_vervoer = self.view.entry_vervoer_type.get()
+        if not type_vervoer: return self.view.show_error("Vul een type in.")
+        self.model.add_transport(type_vervoer)
+        self.view.entry_vervoer_type.delete(0, 'end')
+        self.refresh_all_tables()
+        self.view.show_info("Vervoer toegevoegd.")
+
+    def delete_transport(self):
+        trans_id = self.view.get_selected_id(self.view.tree_trans)
+        if not trans_id: return self.view.show_error("Selecteer vervoer.")
+        self.model.delete_transport(trans_id)
+        self.refresh_all_tables()
+        self.view.show_info("Vervoer en bijbehorende logs verwijderd.")
+
+    # --- Logs ---
+    def add_log(self):
+        student_val = self.view.combo_student.get()
+        vervoer_val = self.view.combo_vervoer.get()
+        datum = self.view.entry_datum.get()
+
+        if not student_val or not vervoer_val or not datum:
+            return self.view.show_error("Vul alle log-velden in!")
+
+        # Haal de ID's uit de string (bv "1 - Jan Peeters" -> 1)
+        student_id = int(student_val.split(" - ")[0])
+        vervoer_id = int(vervoer_val.split(" - ")[0])
+
+        self.model.add_log(student_id, vervoer_id, datum)
+        self.view.entry_datum.delete(0, 'end')
+        self.refresh_all_tables()
+        self.view.show_info("Verplaatsing opgeslagen.")
+
+    def delete_log(self):
+        log_id = self.view.get_selected_id(self.view.tree_logs)
+        if not log_id: return self.view.show_error("Selecteer een log.")
+        self.model.delete_log(log_id)
+        self.refresh_all_tables()
+        self.view.show_info("Log verwijderd.")
 
     def start(self):
         self.view.mainloop()
